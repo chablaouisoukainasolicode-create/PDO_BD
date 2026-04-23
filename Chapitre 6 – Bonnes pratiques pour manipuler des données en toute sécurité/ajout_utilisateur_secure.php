dc0d72a6b957b245f598e5fc013e04875705f3b7
chablaouisoukainasolicode-create <chablaoui.soukaina.solicode@gmail.com>
2026-04-23 14:03:34 +0000

    Add files via upload

---
+++ Chapitre 6 – Bonnes pratiques pour manipuler des données en toute sécurité/ajout_utilisateur_secure.php
@@ -0,0 +1,38 @@
+<?php
+require 'connexion.php';
+
+$nom = htmlspecialchars(trim($_POST['nom']));
+$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+if (!$email) {
+    die("Email invalide !");
+}
+
+$stmt = $pdo->prepare("INSERT INTO Utilisateur (nom, email) VALUES (:nom, :email)");
+$stmt->execute([
+    'nom' => $nom,
+    'email' => $email
+]);
+echo "Utilisateur ajouté avec succès.";
+try {
+    // Code d’insertion
+} catch (PDOException $e) {
+    file_put_contents('logs/errors.log', $e->getMessage(), FILE_APPEND);
+    echo "Une erreur est survenue. Contactez l’administrateur.";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+    <input type="text" name="nom" placeholder="Nom" required>
+    <input type="email" name="email" placeholder="Email" required>
+    <button type="submit" name="ok">Ajouter</button>
+</form>
+</body>
+</html>
